7f4663788d71dfca8833ffe7d50b542666f69fb6
Mahavir Chaudhari <mveer.rc@gmail.com>
2021-05-18 18:09:31 +0530

    graphs

---
+++ BasiclevelQuestions/Graphs.py
@@ -0,0 +1,15 @@
+# Graph theory overview 
+# A graph has (nodes and connections) or (vertices and edges)
+
+# Tree is a acyclic and undirected graph
+
+# Adj list for each node, list of which nodes are connected
+# Space O(V+E) time O(V)
+
+# adj matrix (1 or 0) for has a edge or nah
+# Space O(v²), time O(1)
+
+# traversals-
+# DFS - RECURSIVE - STACK
+# BFS - ITERATIVE - QUEUE
+
