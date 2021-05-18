ddd4e7f674cc8b89ff4e18bc127e6a33a05205b8
Mahavir Chaudhari <mveer.rc@gmail.com>
2021-05-18 19:35:57 +0530

    graphs

---
+++ BasiclevelQuestions/AdMatrixTraversal.py
@@ -0,0 +1,34 @@
+# adj matrix in directed, undirected, weighted graphs
+
+def printmatrix(matrix):
+    r,c = len(matrix),len(matrix[0])
+    for i in range(r):
+        for j in range(c):
+            print(matrix[i][j],end="\t")
+        print()
+
+
+# input
+v,e = map(int,input().split())
+matrix = [[0]*v for i in range(v)]
+
+
+'''
+for i in range(e):
+    u,v = map(str,input().split())
+    u = ord(u) - ord('A')
+    v = ord(v)  - ord('A')
+    matrix[u][v] = 1 # True or False in case of edge
+    matrix[v][u] = 1
+'''
+
+#for weighted
+for i in range(e):
+    u,v,w = map(str,input().split())
+    u = ord(u) - ord('A')
+    v = ord(v) - ord('A')
+    w = int(w)
+    matrix[u][v] = w # Weight / Cost in case of edge
+    
+
+printmatrix(matrix)
